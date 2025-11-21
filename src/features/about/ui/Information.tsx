d56b0e9cda724b7cee936ae8b97ecc4a9ee12a0a
dgrimaldi92 <grimi.david@gmail.com>
2025-11-21 20:42:59 +0100

    feat: chage information text

--- src/features/about/ui/Information.tsx
+++ src/features/about/ui/Information.tsx
@@ -4,19 +4,19 @@ export function Information(): JSX.Element {
 	return (
 		<div class="min-h-screen w-full p-25 sm:p-50 sm:text-2xl">
 			<p class="font-bold">
+				Since 2016, when Bernava developed the first land art project supported
+				by public and private stakeholders, he is working on a personal research
+				aims not to produce new objects but new ways of seeing what already
+				exists, blending tradition and avant-garde.
+			</p>
+			<br />
+			<br />
+			<p class="font-bold">
 				His practice gives a central role to observation by placing it at the
 				center of artistic practice. It aims to stimulate care for the landscape
 				through its continuous rethinking in a contemporary key, without
 				altering its profile, but highlighting its specific beauty through the
 				symbolism of color.
-			</p>
-			<br />
-			<br />
-			<p class="font-bold">
-				Since 2016, when Bernava developed the first land art project supported
-				by public and private stakeholders, he is working on a personal research
-				aims not to produce new objects but new ways of seeing what already
-				exists, blending tradition and avant-garde.
 			</p>
 		</div>
 	);
